2c94ca294b4122f4e39631f2d7042034a1a34115
Aaron Shaw <shawaj@gmail.com>
2021-08-07 21:33:13 +0100

    add listen address env variable
    add listen address env variable

--- start-gatewayrs.sh
+++ start-gatewayrs.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+if ! LISTEN_ADDR=$(/bin/hostname -i)
+then
+  exit 1
+else
+  export GW_LISTEN_ADDR="${LISTEN_ADDR}:1680"
+fi
+
 /usr/bin/helium_gateway -c /etc/helium_gateway server
