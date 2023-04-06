05c94f482d6cf80185966f65d9eb4965407c2024
Murat Ursavas <mursavas@nebra.com>
2023-04-06 18:53:33 +0300

    Added precautionary empty region check

--- gen-region.sh
+++ gen-region.sh
@@ -7,7 +7,7 @@ done
 
 REGIONDATA=$(helium_gateway info region | grep "region" | cut -d ":" -f2 | tr -d " ,\"")
 
-while [ "$REGIONDATA" = "EU433" ]; do
+while [ "$REGIONDATA" = "EU433" ] || [ "$REGIONDATA" = "" ]; do
     sleep 1
     REGIONDATA=$(helium_gateway info region | grep "region" | cut -d ":" -f2 | tr -d " ,\"")
 done
